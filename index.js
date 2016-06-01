98f770c3668cf53d4cdbd9970fad9abfe0eabcf0
Paul Watkins <paul.watkins@croud.co.uk>
2016-06-01 10:15:23 +0100

    add check for UK format prior to crono check (crono defaults to international)

--- index.js
+++ index.js
@@ -1,25 +1,24 @@
 
-module.exports = function (value, comparisonDate, option) {
 
-    var chrono = require('chrono-node')
+const parseUkFormat = function(value) {
 
-    var period_pattern = /([+-])([0-9]*)\W(months|weeks|days|years|week|day|month|year)/ig
+    var date_pattern = /([0-9]{1,2})([\\/\\.\\-])([0-9]{1,2})([\\/\\.\\-])([0-9]{4}|[0-9]{2})/i,
+    myRegexDate = new RegExp(date_pattern),
+    dateMatch = myRegexDate.test(value),
+    matches;
 
-    var myRegex = new RegExp(period_pattern),
-    matches,
-    return_date = null,
-    chrono_date;
-
-    if (value == '') {
-        value = 'today';
+    if (matches = myRegexDate.exec(value)) {
+        return moment(value, `DD${matches[2]}MM${matches[4]}YYYY`);
     }
+    return null
+}
 
-    if (myRegex.test(value)) {
+const parsePlusPeriods = function(value, comparisonDate) {
+    var period_pattern = /([+-])([0-9]*)\W(months|weeks|days|years|week|day|month|year)/ig
+    var myRegex = new RegExp(period_pattern),
+    matches;
 
-        myRegex = new RegExp(period_pattern);
-
-        matches = myRegex.exec(value);
-
+    if (matches = myRegex.exec(value)) {
         if (matches && matches.length) {
             if (matches[1] == '-') {
                 return comparisonDate.subtract(parseInt(matches[2]), matches[3]);
@@ -28,6 +27,13 @@ module.exports = function (value, comparisonDate, option) {
             }
         }
     }
+    return null;
+}
+
+const parseChrono = function(value) {
+    var chrono = require('chrono-node'),
+    return_date = null,
+    chrono_date;
 
     chrono_date = chrono.parse(value);
 
@@ -37,5 +43,20 @@ module.exports = function (value, comparisonDate, option) {
     }
 
     return return_date;
+}
+
+module.exports = function (value, comparisonDate, option) {
+
+    if (value == '') {
+        value = 'today';
+    }
+
+    var ukFormatDate = parseUkFormat(value);
+    if (ukFormatDate) return ukFormatDate;
+
+    var plusPeriods = parsePlusPeriods(value, comparisonDate);
+    if (plusPeriods) return plusPeriods;
+
+    return parseChrono(value);
 
 };
